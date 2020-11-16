2617d4037595b16b2ac5741f7340fa191eae586f
Ryo Munakata <afpacket@gmail.com>
2020-11-17 00:47:54 +0900

    Read revision

--- enc28j60kai.c
+++ enc28j60kai.c
@@ -1,5 +1,8 @@
 #include <linux/module.h>
 #include <linux/spi/spi.h>
+#include <linux/netdevice.h>
+#include <linux/etherdevice.h>
+#include <linux/delay.h>
 
 #define DRIVER_NAME "enc28j60kai"
 
@@ -26,7 +29,7 @@ struct control_register {
 		.address = (a) \
 	}
 
-/* Common Register Set */
+/* Common registers */
 const struct control_register EIE	= CONTROL_REGISTER(BANK_COMMON, 0x1b);
 const struct control_register EIR	= CONTROL_REGISTER(BANK_COMMON, 0x1c);
 const struct control_register ESTAT	= CONTROL_REGISTER(BANK_COMMON, 0x1d);
@@ -36,6 +39,9 @@ const struct control_register ECON1	= CONTROL_REGISTER(BANK_COMMON, 0x1f);
 /* Bank 3 */
 const static struct control_register EREVID = CONTROL_REGISTER(BANK_3, 0x12);
 
+#define ECON1_BSEL1 0x02
+#define ECON1_BSEL0 0x01
+
 enum spi_command {
 	SPI_COM_RCR = 0x0,
 	SPI_COM_RBM = 0x1,
@@ -43,25 +49,36 @@ enum spi_command {
 	SPI_COM_WBM = 0x3,
 	SPI_COM_BFS = 0x4,
 	SPI_COM_BFC = 0x5,
-	SPI_COM_SRC = 0x7,
+	SPI_COM_SRC = 0xff,
 };
 
 union spi_operation {
 	uint8_t raw8;
 	struct __attribute__ ((packed)) {
-		uint8_t opcode:3;
 		uint8_t arg:5;
+		uint8_t opcode:3;
 	} op;
 };
 
+struct enc_driver_data {
+	struct net_device *netdev;
+	struct spi_device *spi;
+};
+
+static void enc_soft_reset(struct spi_device *spi)
+{
+	spi_w8r8(spi, SPI_COM_SRC);
+	udelay(2000);
+}
+
 static void enc_set_bank(struct spi_device *spi, enum bank_number bank);
 
-static uint8_t spi_enc_rcr(struct spi_device *spi, struct control_register reg)
+static uint8_t spi_enc_read(struct spi_device *spi, enum spi_command com, struct control_register reg)
 {
 	union spi_operation spi_op = {
 		.op = {
 			.arg = reg.address,
-			.opcode = SPI_COM_RCR,
+			.opcode = com,
 		}
 	};
 
@@ -78,35 +95,59 @@ static void spi_enc_write(struct spi_device *spi, enum spi_command com, struct c
 			.opcode = com,
 		}
 	};
+	uint8_t txbuf[2] = {spi_op.raw8, data};
+	uint8_t rxbuf[1];
 
 	enc_set_bank(spi, reg.bank);
 
-	spi_w8r8(spi, spi_op.raw8);
-	spi_w8r8(spi, data);
+	spi_write_then_read(spi, txbuf, 2, rxbuf, 1);
 }
-
-#define ECON1_BSEL1 0x02
-#define ECON1_BSEL0 0x01
 
 static void enc_set_bank(struct spi_device *spi, enum bank_number bank)
 {
 	if (bank != BANK_COMMON) {
-		uint8_t econ1 = spi_enc_rcr(spi, ECON1);
 		spi_enc_write(spi, SPI_COM_BFC, ECON1, ECON1_BSEL1 | ECON1_BSEL0);
-		spi_enc_write(spi, SPI_COM_BFS, ECON1, (uint8_t)bank);
-		dev_info(&spi->dev, "enc_set_bank: econ1=%x set=%x", econ1, (econ1 & 0b11111100) | (uint8_t)bank);
+		spi_enc_write(spi, SPI_COM_BFS, ECON1, bank);
 	}
 }
 
 static int enc_probe(struct spi_device *spi)
 {
-	dev_info(&spi->dev, "%s: probe: revision=%d", DRIVER_NAME, spi_enc_rcr(spi, EREVID));
+	int revision;
+	struct net_device *netdev;
+	struct enc_driver_data *priv;
+
+	netdev = alloc_etherdev(sizeof (struct enc_driver_data));
+	if (!netdev)
+		goto err_alloc;
+
+	priv = netdev_priv(netdev);
+	priv->netdev = netdev;
+	priv->spi = spi;
+	spi_set_drvdata(spi, priv);
+
+	SET_NETDEV_DEV(netdev, &spi->dev);
+
+	enc_soft_reset(spi);
+
+	revision = spi_enc_read(spi, SPI_COM_RCR, EREVID);
+	dev_info(&spi->dev, "probe: revision=%d", revision);
+
+	if (revision == 0 || revision == 0xff) {
+		return -EINVAL;
+	}
+
 	return 0;
+
+err_alloc:
+	return -ENOMEM;
 }
 
 static int enc_remove(struct spi_device *spi)
 {
-	dev_info(&spi->dev, "%s: remove", DRIVER_NAME);
+	struct enc_driver_data *priv = spi_get_drvdata(spi);
+
+	dev_info(&priv->spi->dev, "%s: remove", DRIVER_NAME);
 	return 0;
 }
 
